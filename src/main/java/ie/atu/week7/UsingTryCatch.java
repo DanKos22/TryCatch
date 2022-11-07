5c99948c310d19a5b7bfd93aef42c670fe14575e
Dan Koskiranta <G00397054@atu.ie>
2022-11-07 10:44:59 +0000

    Added some feature

--- src/main/java/ie/atu/week7/UsingTryCatch.java
+++ src/main/java/ie/atu/week7/UsingTryCatch.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        try
-        {
+
             System.out.println("Please enter a number between 0 - 9: ");
             Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
-            if(myNum <= 9 || myNum > 0)
+            boolean valid = false;
+
+            while(valid == false)
             {
-                System.out.println("You entered " + myNum);
+                try
+                {
+                    int myNum = myscan.nextInt();
+                    if(myNum <= 9 || myNum > 0)
+                    {
+                        System.out.println("You entered " + myNum);
+                        valid = true;
+                    }
+                    else
+                    {
+                        System.out.println("Not a valid number");
+                        valid = false;
+                    }
+                }
+                catch(InputMismatchException e)
+                {
+                    System.out.println("Please enter a number between 0 - 9:");
+                    myscan.next();
+                    continue;
+                }
             }
-            else
-            {
-                System.out.println("Not a valid number");
-            }
-        }
-        catch(InputMismatchException e)
-        {
-            System.out.println("Please enter a value between 0 - 9: ");
-        }
+
     }
 }
